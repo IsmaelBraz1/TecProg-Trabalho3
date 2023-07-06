98cd6d9ca567dfa0cb704db662ad56752f54c56d
Pedro Rickson <pedrorickson95.fa@gmail.com>
2023-07-06 20:31:55 -0300

    Adicionando mais interação gráfica as cartas (ênfase e enable)

--- Trabalho3/src/View/include/Card.java
+++ Trabalho3/src/View/include/Card.java
@@ -13,18 +13,28 @@ import listeners.CardListener;
 
 public class Card extends JLabel {
 	private ImageIcon cardImage;
-	private final Color colorOverlaySelect = new Color(0, 0, 0, 150);
+	private Image image;
+	private CardListener listener;
+	private Dimension cardSize;
 	private boolean mark;
 	private boolean lock;
+	private boolean enabled;
 	
 	public Card(String imageFilePath) {
-		this.lock(true);
 		ImageIcon imageCard = new ImageIcon(imageFilePath);
-		this.cardImage = new ImageIcon(imageCard.getImage().getScaledInstance(150, 240, java.awt.Image.SCALE_SMOOTH ));
-		this.addMouseListener(new CardListener(imageFilePath,this));
-		this.setIcon(this.cardImage);
+		try {
+			this.image = ImageIO.read(new File(imageFilePath));
+		} catch (Exception e) {
+			System.out.println("Problema ao ler a imagem da carta");
+			return;
+		}
+		this.listener = new CardListener(imageFilePath,this);
+		this.cardSize = new Dimension(150, 240);
+		this.setIcon(new ImageIcon(image.getScaledInstance((int)this.cardSize.getWidth(), (int)this.cardSize.getHeight(), java.awt.Image.SCALE_SMOOTH )));
+		this.lock(true);	
+		this.emphasis(false);
 		this.repaint();
-	
+		
 	}
 	
 	public void mark(boolean state) {
@@ -32,27 +42,47 @@ public class Card extends JLabel {
 		this.repaint();
 	}
 	
+	public void SetEnabled (boolean state) {
+		if(state) {
+			this.enabled = true;
+			this.addMouseListener(this.listener);
+			this.setCursor(new Cursor(Cursor.HAND_CURSOR));
+		}else {
+			this.enabled = false;
+			this.addMouseListener(null);
+			this.setCursor(new Cursor(Cursor.DEFAULT_CURSOR));
+		}	
+	}
+	
+	public void emphasis(boolean state) {
+		if(state) {
+			this.cardSize = new Dimension(150, 240);
+		}else {
+			this.cardSize = new Dimension(113,180);
+		}
+		
+		this.setIcon(new ImageIcon(image.getScaledInstance((int)this.cardSize.getWidth(), (int)this.cardSize.getHeight(), java.awt.Image.SCALE_SMOOTH )));
+		this.repaint();
+	}
+	
 	
 	public void lock(boolean state) {
 		this.lock = state;
-		if(state) {
-			this.setCursor(new Cursor(Cursor.DEFAULT_CURSOR));
-		}else {
-			this.setCursor(new Cursor(Cursor.HAND_CURSOR));
-		}
+		this.SetEnabled(!state);
 		this.repaint();
 	}
 	
 	@Override
 	protected void paintComponent(Graphics g) {
-		// TODO Auto-generated method stub
 		super.paintComponent(g);
+		
+		final Color colorOverlaySelect = new Color(0, 0, 0, 150);
 		Graphics2D g2 = (Graphics2D) g.create();
 		if(lock) {
 			g2.drawImage(this.lockImageOverlay(), 0, 0, null);
 		}else {
 			if(mark) {
-				 g2.setColor(this.colorOverlaySelect);
+				 g2.setColor(colorOverlaySelect);
 			     g2.fillRect(0, 0, getWidth(), getHeight());
 			}
 		}
@@ -71,12 +101,12 @@ public class Card extends JLabel {
 			
 			for(int i=0;i<height*width;i++) {
 				int pixel = imagePixels[i];
-				pixel = pixel & 0x80ffffff;
+				pixel = pixel & 0xa0ffffff;
 				imagePixels[i] = pixel;
 			}
 			bufImage.setRGB(0, 0, width, height, imagePixels, 0, width);
 		
-			return bufImage.getScaledInstance(150, 240, BufferedImage.SCALE_SMOOTH);
+			return bufImage.getScaledInstance((int)this.cardSize.getWidth(), (int)this.cardSize.getHeight(), BufferedImage.SCALE_SMOOTH);
 		} catch (Exception e) {
 			return null;
 		}
