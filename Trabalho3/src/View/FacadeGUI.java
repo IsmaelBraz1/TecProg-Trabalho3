4fab108c2924adfd6382fd1f1f4851aa2f1420a6
Pedro Rickson <pedrorickson95.fa@gmail.com>
2023-07-07 15:06:31 -0300

    Facade esqueleto

--- Trabalho3/src/View/FacadeGUI.java
+++ Trabalho3/src/View/FacadeGUI.java
@@ -1,5 +1,34 @@
 package View;
 
-public class FacadeGUI {
 
+public class FacadeGUI {
+	private TelaPrincipal screen;
+	
+	public FacadeGUI(TelaPrincipal screen) {
+		this.screen = screen;
+	}
+	
+	public void nameWelcomeScreen() {
+		
+	}
+	
+	public void connectAndHostScreen() {}
+	
+	public void waitingGameScreen() {}
+	
+	public void GameScreen() {}
+	
+	public void unlockSelectCards() {}
+	
+	public void setCards() {}
+	
+	public void setMessage() {}
+	
+	public void setPoints() {}
+	
+	public void setName() {}
+	
+	public void VotationScreen() {}
+	
+	public void RevealCard() {}
 }
