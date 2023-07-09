4f340d8b453a7e9e7bf60e882880e5a349d40e18
Pedro Rickson <pedrorickson95.fa@gmail.com>
2023-07-09 01:52:35 -0300

    Adicionando listeners nos botões de host/join

--- Trabalho3/src/View/include/MultiplayerSetup.java
+++ Trabalho3/src/View/include/MultiplayerSetup.java
@@ -7,6 +7,8 @@ import java.awt.Insets;
 import javax.swing.JButton;
 
 import View.style.StylePanel;
+import listeners.HostAGame;
+import listeners.JoinAGame;
 
 public class MultiplayerSetup extends StylePanel {
 	
@@ -18,9 +20,11 @@ public class MultiplayerSetup extends StylePanel {
 		c.anchor = GridBagConstraints.WEST;
 		c.weightx  =2;
 		JButton hostGameButton = new JButton("Host o jogo");
+		hostGameButton.addActionListener(new HostAGame());
 		this.add(hostGameButton,c);
 		c.gridy = 1;
 		JButton joinGameButton = new JButton("Junte-se a um jogo");
+		joinGameButton.addActionListener(new JoinAGame());
 		this.add(joinGameButton,c);
 	}
 }
